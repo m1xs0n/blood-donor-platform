b6651cd5999ed2cae42288a66935da3a89b382b3
Sherehiy <sherehiy@example.com>
2026-05-30 22:36:10 +0300

    update project9

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,10 @@ app.get('/', (req, res) => {
     res.send('Сервер працює!');
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Сервер запущено на порту ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущено на порту ${PORT}`);
 });
 
 const donorRoutes =
